962152e498a5ef4f3d22bfd66e0fa7517789ba00
Adam Sanchez <a.sanchez75@gmail.com>
2017-12-08 14:13:14 +0100

    php5.6 implementation

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM asanchez75/symfony:nodejs
+FROM asanchez75/php5.6-symfony:latest
 RUN mkdir /data && \
     cd /data && \
-    git clone http://github.com/conjecto/nemrod-demo && \
-    cd /data/nemrod-demo && \
+    git clone http://github.com/conjecto/nemrod-demo web && \
+    cd /data/web && \
     composer install
 ADD init.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/init.sh
--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-cp -Rf /data/nemrod-demo/* /var/www/html/
-cd /var/www/html/ && php app/console server:start 0.0.0.0:8009
+# /vhost/current/web is the wwwroot folder
+cp -Rf /data/web /vhost/current/
+cd /vhost/current/web && php app/console server:start 0.0.0.0:8009
 #/usr/local/bin/
-exec /usr/sbin/init # To correctly start D-Bus thanks to https://forums.docker.com/t/any-simple-and-safe-way-to-start-services-on-centos7-systemd/5695/8
+#exec /usr/sbin/init # To correctly start D-Bus thanks to https://forums.docker.com/t/any-simple-and-safe-way-to-start-services-on-centos7-systemd/5695/8
+while true; do sleep 1d; done
